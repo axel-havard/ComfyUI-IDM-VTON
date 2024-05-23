0d3ff1ecee1cf4afdb44cf8ec9981c08de1a00c6
Axel Havard <axel@uwear.ai>
2024-05-22 21:51:05 -0400

    wip_lower_vram

--- src/nodes/idm_vton_low_vram.py
+++ src/nodes/idm_vton_low_vram.py
@@ -23,6 +23,30 @@ def rescale_noise_cfg(noise_cfg, noise_pred_text, guidance_rescale=0.0):
     noise_cfg = guidance_rescale * noise_pred_rescaled + (1 - guidance_rescale) * noise_cfg
     return noise_cfg
 
+def prepare_text_embeddings(positive,negative=None):
+    text_embeddings = positive[0][0]
+    if negative is not None:
+        text_embeddings = torch.cat([negative[0][0],text_embeddings],dim=0)
+    
+    return text_embeddings
+
+def prepare_pool_embeds(
+    positive,
+    negative=None
+):
+    pool_embeddings = positive[0][1].get("pooled_output")
+    if negative is not None:
+        pool_embeddings = torch.cat([negative[0][1].get("pooled_output"),pool_embeddings],dim=0)
+
+    return pool_embeddings
+
+def sdxl_get_add_time_ids(
+    batch_size,original_size, crops_coords_top_left, target_size, dtype
+):
+    add_time_ids = list(original_size + crops_coords_top_left + target_size)
+    add_time_ids = torch.tensor([add_time_ids], dtype=dtype)
+    add_time_ids = torch.cat([add_time_ids]*batch_size,dim=0)
+    return add_time_ids
 
 class IDM_VTON_low_VRAM:
     @classmethod
@@ -60,17 +84,23 @@ class IDM_VTON_low_VRAM:
         scheduler = DDPMScheduler.from_pretrained(scheduler_path)
         scheduler.set_timesteps(num_inference_steps)
         timesteps = scheduler.timesteps
-        add_text_embeds = None
-        add_time_ids = None
-        latents = noise_latent
+        add_text_embeds = prepare_pool_embeds(positive,negative)
+        height = noise_latent.shape[2] * 8 
+        width = noise_latent.shape[3] * 8
+        original_size=target_size=(height,width)
+        crops_coords_top_left=(0,0)
+        add_time_ids = sdxl_get_add_time_ids(noise_latent.shape[0]*2,tryon_net,original_size, crops_coords_top_left,target_size,dtype=noise_latent.dtype)
+        
         added_cond_kwargs = {"text_embeds": add_text_embeds, "time_ids": add_time_ids}
         added_cond_kwargs["image_embeds"] = tryon_net.encoder_hid_proj(garment_image_clip_embedding)
 
+        latents = noise_latent
+        
         if mask_image_latent.shape[1]>1:
             mask_image_latent = mask_image_latent[:,0:1,:,:]
 
-        text_embeds_cloth = torch.cat([garment_negative,garment_positive],dim=0)
-        prompt_embeds = torch.cat([negative,positive],dim=0)
+        text_embeds_cloth = prepare_text_embeddings(garment_positive,garment_negative)
+        prompt_embeds = prepare_text_embeddings(positive,negative)
         with self.progress_bar(total=num_inference_steps) as progress_bar:
             for i, t in enumerate(timesteps):
                 if self.interrupt:
@@ -81,22 +111,23 @@ class IDM_VTON_low_VRAM:
 
                 latent_model_input = torch.cat([latent_model_input, mask_image_latent, masked_image_latent,densepose_latent], dim=1)
                 
-                _,reference_features = garment_net(garment_image_latent,t, text_embeds_cloth,return_dict=False)
+                with torch.no_grad():
+                    _,reference_features = garment_net(garment_image_latent,t, text_embeds_cloth,return_dict=False)
 
                 reference_features = list(reference_features)
 
                 
                 reference_features = [torch.cat([torch.zeros_like(d), d]) for d in reference_features]
 
-
-                noise_pred = tryon_net(
-                    latent_model_input,
-                    t,
-                    encoder_hidden_states=prompt_embeds,
-                    added_cond_kwargs=added_cond_kwargs,
-                    return_dict=False,
-                    garment_features=reference_features,
-                )[0]
+                with torch.no_grad():
+                    noise_pred = tryon_net(
+                        latent_model_input,
+                        t,
+                        encoder_hidden_states=prompt_embeds,
+                        added_cond_kwargs=added_cond_kwargs,
+                        return_dict=False,
+                        garment_features=reference_features,
+                    )[0]
 
                 noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
                 noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_text - noise_pred_uncond)
